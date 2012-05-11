10d0fb43f2d7d19a00e731f1579290a054776d7f
nojimage <nojimage@gmail.com>
2012-05-11 20:30:22 +0900

    update version

--- models/datasources/twitter_source.php
+++ models/datasources/twitter_source.php
@@ -9,15 +9,15 @@ App::import('vendor', 'TwitterKit.HttpSocketOauth', array('file' => 'http_socket
  * for CakePHP 1.3+
  * PHP version 5.2+
  *
- * Copyright 2010, ELASTIC Consultants Inc. (http://elasticconsultants.com)
+ * Copyright 2012, ELASTIC Consultants Inc. (http://elasticconsultants.com)
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @version    1.0
+ * @version    1.1
  * @filesource
  * @author     nojimage <nojima at elasticconsultants.com>
- * @copyright  2010, ELASTIC Consultants Inc.
+ * @copyright  2012, ELASTIC Consultants Inc.
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link       http://elasticconsultants.com
  * @package    twitter_kit
